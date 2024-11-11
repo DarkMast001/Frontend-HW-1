94cb3d780f6ed80daeb24926592991f6c4d58e5a
Dmitry <demaster.easy@gmail.com>
2024-11-11 19:46:01 +0300

    Выполненное ДЗ

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+function writeToMe(){
+	window.open("https://t.me/dimon992");
+}
+
+function callToMe(){
+	window.location.href = "tel:+79150333375";
+}
+
+function inviteMe(){
+	window.location.href = "mailto:demaster.easy@gmail.com";
+}
